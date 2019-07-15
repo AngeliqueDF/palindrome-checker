ed640f1d222d9ecce36a5f8222b49f25b11287e0
Angelique Faye <faye.angelique.d@gmail.com>
2019-07-15 19:30:29 +0200

    improvements

--- palindrome.js
+++ palindrome.js
@@ -1,26 +1,36 @@
-function palindrome(str) {
+//get launch button
+launchButton = document.getElementById("launchButton");
 
-    var string = str;
+const palindrome = () =>{
+    // var string = document.getElementById("string").value;
+    var string = document.getElementById('stringInput').value;
+    //initialize variable for reversed version of string entered
     var reversedString = "string";
+
+    //boolean that will hold and display palidrome() result on page
+    var result = true;
+    //element that will display result
+    displayResult = document.getElementById("result");
 
     //strip  non-alphanumeric characters
     string = string.replace(/[^A-Za-z0-9]/g, '');
-
+    
     string = string.toLowerCase();
 
     //function to reverse string
     function reverse(s) {
         return s.split('').reverse().join('');
     }
-
     reversedString = reverse(string);
 
-    //check that original and reverted are the same
-    if (string == reversedString) {
-        return true;
+    //display true/false in #result, returned value from palindrome call
+    if (string == reversedString) {//check whether original and reverted are the same
+        result = true
+        displayResult.innerHTML = "true";
     } else {
-        return false;
+        displayResult.innerHTML = "false";
     }
-
 }
-palindrome("toto")
+
+//listen for click event on launch button
+launchButton.addEventListener("click", palindrome);
